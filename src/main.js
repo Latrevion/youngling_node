e7765269f0a5a851f9ac20627ab3abb902d01a6e
luke <xiaopikaqiu@outlook.com>
2025-08-03 23:13:23 +0800

    http json res

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
-const http= require('http');
+const http = require("http");
 
-const server = http.createServer((req,res)=>{
-    res.write('hello the world')
-    res.end()
-})
+const server = http.createServer((req, res) => {
+  const data = {
+    id:1,
+     title:'hello world',
+     description:'this is a description'
+  }
+  const jsonData = JSON.stringify(data)
+  
+  res.writeHead(200,{'content-type':'application/json;charset=utf-8'})
 
-server.listen(3000,()=>{
-    console.log('server is running on port 3000')
-})
+  res.end(JSON.stringify(data));
+});
+
+server.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
