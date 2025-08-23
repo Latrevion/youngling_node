8e6d398c7be26e2efcc374dbf442666e68141f41
lv <xiaopikaqiu@outlook.com>
2025-08-23 14:57:37 +0800

    set res header

--- src/main.js
+++ src/main.js
@@ -2,12 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-
 /**
  * use json middle Middleware
  */
 app.use(express.json());
-
 
 app.listen(port, () => {
   console.log("This server is start");
@@ -40,36 +38,38 @@ const data = [
   },
 ];
 
-app.get('/posts', (req, res) => {
+app.get("/posts", (req, res) => {
   res.send(data);
 });
 
-app.get('/posts/:postId',(req,res)=>{
+app.get("/posts/:postId", (req, res) => {
   //fetch id
-  const {postId} = req.params;
-  
-  //find post 
-  const posts =data.filter((post)=>post.id === postId);
-  
-  //send post
-  res.send(posts[0])
-})
+  const { postId } = req.params;
 
+  //find post
+  const posts = data.filter((post) => post.id === postId);
+
+  //send post
+  res.send(posts[0]);
+});
 
 /**
  * create posts
  */
-app.post('/posts', (req, res) => {
-    //Get the data in the request
-    const {content} = req.body;
+app.post("/posts", (req, res) => {
+  //Get the data in the request
+  const { content } = req.body;
 
-    //set res status
-    res.status(201)
+  //set res status
+  res.status(201);
 
-    console.log(req.headers['sing-along'])
+  console.log(req.headers["sing-along"]);
 
-    //Respond
-    res.send({
-        message:`create successfully ${content}`,
-    })
+  //set res header
+  res.setHeader("Sing-Along", "Yes");
+
+  //Respond
+  res.send({
+    message: `create successfully ${content}`,
+  });
 });
