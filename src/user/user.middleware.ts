379a8de0a8e99a94cad93f36d3926dfd6e87075e
lv <xiaopikaqiu@outlook.com>
2025-09-08 10:18:56 +0800

    Validate user data

--- src/user/user.middleware.ts
+++ src/user/user.middleware.ts
@@ -1,2 +1,19 @@
 import {Request,Response,NextFunction} from 'express';
 import * as userService from './user.service';
+
+
+/**
+ * Validate user data
+ */
+export const validateUserData = (req: Request, res: Response, next: NextFunction) => {
+  console.log('validateUserData');
+
+  //prepare data
+  const {name ,password} = req.body;
+
+  //Validate required data
+  if(!name)return next(new Error('name required'));
+  if(!password) return next(new Error('password required'));
+
+  next();
+}
--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -1,12 +1,13 @@
 import express, { Router } from 'express';
 import * as userController from './user.controller';
+import  {validateUserData} from './user.middleware';
 
 const router:Router = express.Router();
 
 /**
  * create user
  */
-router.post('/users',userController.store);
+router.post('/users',validateUserData,userController.store);
 
 /**
  * export router
