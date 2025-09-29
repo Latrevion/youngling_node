678aa0365e37d8ed22da1f74f1f67b9fd2bfe9f5
lv <xiaopikaqiu@outlook.com>
2025-09-29 19:29:23 +0800

    Key configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -18,3 +18,14 @@ export const {
   MYSQL_PASSWORD,
   MYSQL_DATABASE,
 }=process.env
+
+
+/**
+ * Key configuration
+ */
+export let { PUBLIC_KEY, PRIVATE_KEY } = process.env;
+// @ts-ignore
+PUBLIC_KEY = Buffer.from(PUBLIC_KEY, 'base64').toString();
+// @ts-ignore
+PRIVATE_KEY=Buffer.from(PRIVATE_KEY,'base64').toString();
+
